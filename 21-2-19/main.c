d05fc4c2744648245d55887ca8008e5e00a3fbbc
ganwang <74584251+ganwang-bit@users.noreply.github.com>
2021-02-19 20:29:29 +0800

    21-2-19

---
+++ 21-2-19/main.c
@@ -0,0 +1,57 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//    printf("Hello world!\n");
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    perror("sdfa");
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    int a=10;
+//    fwrite(&a,4,1,stdout);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    int a;
+//    scanf("%2d",&a);
+//    printf("%d",a);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    char arr[10];
+//    scanf("%2s",arr);
+//    printf("%s",arr);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    printf("%e",6.132);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    char arr[10];
+//    scanf("%[abc]",arr);
+//    printf("%s",arr);
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    printf("%[abc]","bba,");
+//    return 0;
+//}
